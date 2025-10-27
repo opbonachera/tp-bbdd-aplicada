d0c76d77c3232cebdc226516eb84be69bd719886
opbonachera <obonachera@gmail.com>
2025-10-27 16:34:38 -0300

    agrego funcion para eliminar espacios

---
+++ funciones/limpiar-espacios.sql
@@ -0,0 +1,16 @@
+CREATE OR ALTER FUNCTION ddbba.fn_limpiar_espacios (@valor VARCHAR(MAX))
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+--- Limpia los espacios de una cadena de caracteres
+    DECLARE @resultado VARCHAR(MAX) = @valor;
+
+    SET @resultado = REPLACE(@resultado, CHAR(32), '');
+    SET @resultado = REPLACE(@resultado, CHAR(160), '');
+    SET @resultado = REPLACE(@resultado, CHAR(9), '');
+    SET @resultado = REPLACE(@resultado, CHAR(10), '');
+    SET @resultado = REPLACE(@resultado, CHAR(13), '');
+
+    RETURN @resultado;
+END
+GO
