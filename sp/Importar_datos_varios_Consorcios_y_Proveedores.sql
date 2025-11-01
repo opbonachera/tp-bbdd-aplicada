20bba7b48a09ac7c425a13aea1fa8be63261f9f3
JimeBenitezz <jimebenitezuni@gmail.com>
2025-11-01 20:34:27 -0300

    Agrego como exportar la hoja de Proveedores
    Added stored procedure to import multiple providers from an Excel file into the database.

--- sp/Importar_datos_varios_Consorcios_y_Proveedores.sql
+++ sp/Importar_datos_varios_Consorcios_y_Proveedores.sql
@@ -60,8 +60,65 @@ EXEC ddbba.sp_importar_consorcios
 
 --Para ver si se inserto todo correctamente
 select * from ddbba.consorcio
+		
+------------------------------------------------------------------------------------------------------------------------
+----SP PARA Importar_datos_varios_proveedores
 
+CREATE OR ALTER PROCEDURE ddbba.sp_importar_proveedores
+	@NomArch varchar(255)
+AS
+BEGIN
+ 
+--creacion de la tabla temporal
+ CREATE TABLE #temp_proveedores
+ (  tipo_de_gasto VARCHAR(50),
+	descripcion VARCHAR (100),
+	detalle VARCHAR(100) NULL,
+	nombre_consorcio VARCHAR (255),
+  );
+--inserto los datos del archivo excel a la tabla temporal con openrowset(lee datos desde un archivo)
+--Uso sql dinamico
+   DECLARE @sql NVARCHAR(MAX);
 
+    SET @sql = N'
+        INSERT INTO #temp_proveedores (tipo_de_gasto,descripcion,detalle,nombre_consorcio)
+        SELECT F1 as tipo_de_gasto, F2 as descripcion,F3 as detalle, F4 as nombre_consorcio
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @NomArch + ';HDR=NO'',
+            ''SELECT * FROM [Proveedores$]''
+        );
+    ';
+
+--ejecuto el sql dinamico
+    EXEC sp_executesql @sql;
+
+--Inserto los datos en la tabla original
+	INSERT INTO ddbba.Proveedores(
+	tipo_de_gasto,
+	descripcion,
+	detalle,
+	nombre_consorcio
+	)
+	select
+	t.tipo_de_gasto,
+	t.descripcion,
+	t.detalle,
+	t.nombre_consorcio
+
+	from #temp_proveedores as t
+
+	--elimino la tabla temporal
+	DROP TABLE #temp_proveedores
+END
+GO
+
+--Para ejecutar el SP
+EXEC ddbba.sp_importar_proveedores
+	@NomArch='C:\Importar_TP\datos varios.xlsx' --aca va la ruta de donde tengan el archivo
+
+--Para ver si se inserto todo correctamente
+select * from ddbba.proveedores
 -------------------------------------------------------------------------------------------------------------------------------
 --ESTO ES POR SI ALGO NO LES ANDA
 --Habilita opciones avanzadas
