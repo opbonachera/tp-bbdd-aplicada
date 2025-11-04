2ecaf189bbc8702ba10350cbe1f1d447520d4e1a
perezolivia <ocperez07@gmail.com>
2025-11-03 23:37:47 -0300

    mejoro fn_normalizar_monto.sql
    cambios agregados:
    -contempla casos de montos de 2 digitos o menos
    -contempla casos sin separador (ej 123456)
    -verifica si tiene o no separador con tipo bit (1,0)
    -comentarios para + claridad

--- funciones/fn_normalizar_monto.sql
+++ funciones/fn_normalizar_monto.sql
@@ -1,31 +1,43 @@
 CREATE OR ALTER FUNCTION ddbba.fn_normalizar_monto (@valor VARCHAR(50))
-RETURNS DECIMAL (12,2)
+RETURNS DECIMAL(12,2)
 AS
 BEGIN
 
 /* En esta funcion recibimos un valor monetario y lo convertimos en decimal(12,2), siguiendo estas reglas:
-1) Limpiamos simbolos, espacios, y comas/puntos
-2) Identificamos separador decimal (es coma o punto?)
-3) Eliminamos el separador de miles y cambiamos decimal a punto
-4) Devolvemos el numero normalizado
+1) Limpiamos simbolos y espacios (caracteres no deseados)
+2) Detectamos si tiene separador decimal
+3) Eliminamos todos los separadores
+4) Si tenia separador, insertamos el punto decimal
+5) Devolvemos el numero normalizado
 */
 
-DECLARE @resultado NVARCHAR(50), @entera NVARCHAR(50), @decimal NVARCHAR(10);
+    DECLARE @resultado NVARCHAR(50);
+    DECLARE @tieneSeparador BIT;
 
---1) Limpiamos caracteres no deseados
-SET @resultado = ddbba.fn_limpiar_espacios (LTRIM(RTRIM(ISNULL(@valor, '')))); --borra espacios izq, der y entre medio
-SET @resultado = REPLACE(@resultado, '$', ''); --saca $ (si lo tuviese)
-SET @resultado = REPLACE(@resultado, ',', '');
-SET @resultado = REPLACE(@resultado, '.', '');
---1) Agrega el punto decimal
-SET @resultado = STUFF(@resultado,len(@resultado)-1,0,'.');
-RETURN ISNULL(TRY_CAST(@resultado AS DECIMAL(12,2)), 0.00); --trata de castear el texto a decimal, si no puede, devuelve null y lo transformamos a 0.00
+    -- 1) Limpiamos caracteres no deseados
+    SET @resultado = ddbba.fn_limpiar_espacios(LTRIM(RTRIM(ISNULL(@valor, '')))); --Borra espacios izq, der y entre medio
+    SET @resultado = REPLACE(@resultado, '$', ''); --Saca el $ (si lo tuviese)
+
+    -- 2) Detectamos si tiene separador decimal
+    SET @tieneSeparador = CASE 
+                            WHEN CHARINDEX(',', @resultado) > 0 OR CHARINDEX('.', @resultado) > 0 --CHARINDEX nos busca la primer aparicion del caracter, si es > 0 -> quiere decir que hay por lo menos UNO de los separadores (ya sea coma o punto)
+                            THEN 1 
+                            ELSE 0 
+                          END;
+
+    -- 3) Eliminamos todos los separadores
+    SET @resultado = REPLACE(@resultado, ',', '');
+    SET @resultado = REPLACE(@resultado, '.', '');
+
+    -- 4) Si tenia separador, insertamos el punto decimal
+    IF @tieneSeparador = 1 AND LEN(@resultado) > 2 --En el caso de que tenga tres digitos o mas,
+        SET @resultado = STUFF(@resultado, LEN(@resultado) - 1, 0, '.'); --apuntamos a la posicion justo antes de los ultimos dos digitos (asumimos dos digitos decimales)
+    --Si el numero tiene uno o dos digitos, entonces no entra al if y cuando castee solo le agrega el .00
+
+    -- 5) Devolvemos el número normalizado
+    RETURN ISNULL(TRY_CAST(@resultado AS DECIMAL(12,2)), 0.00); --Trata de castear el texto a decimal, si no puede, devuelve null y lo transformamos a 0.00
 END
 GO
-
-
-
-
 
 
 
