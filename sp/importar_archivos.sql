0d1c6e46bdfe2ca34216d0248843e3ca303101af
opbonachera <obonachera@leafnoise.io>
2025-11-04 21:02:07 -0300

    agrego sp master

--- sp/importar_archivos.sql
+++ sp/importar_archivos.sql
@@ -2,13 +2,12 @@
 create or alter procedure ddbba.sp_importar_archivos
 as
 begin	
-	exec ddbba.sp_importar_consorcios_csv @ruta_archivo = 'C:\Users\leafnoise\Documents\Ornella\Proyectos\tp-bbdd-aplicada\documentacion\Archivos para el TP\datos-varios-consorcios.csv'
-	exec ddbba.sp_importar_pagos @ruta_archivo = 'C:\Users\leafnoise\Documents\Ornella\Proyectos\tp-bbdd-aplicada\documentacion\Archivos para el TP\pagos_consorcios.csv'
-	exec ddbba.sp_importar_uf_por_consorcios @ruta_archivo = 'C:\Users\leafnoise\Documents\Ornella\Proyectos\tp-bbdd-aplicada\documentacion\Archivos para el TP\UF por consorcio.txt'
-	exec ddbba.sp_importar_inquilinos_propietarios @ruta_archivo = 'C:\Users\leafnoise\Documents\Ornella\Proyectos\tp-bbdd-aplicada\documentacion\Archivos para el TP\Inquilino-propietarios-datos.csv'
+	exec ddbba.sp_importar_consorcios_csv @ruta_archivo =
+	exec ddbba.sp_importar_pagos @ruta_archivo =
+	exec ddbba.sp_importar_uf_por_consorcios @ruta_archivo = 
+	exec ddbba.sp_importar_inquilinos_propietarios @ruta_archivo =
 	--exec ddbba.sp_importar_servicios @ruta_archivo = '/var/opt/mssql/tp/Servicios.Servicios.json', @anio=2025
-	exec ddbba.sp_relacionar_inquilinos_uf  @ruta_archivo = 'C:\Users\leafnoise\Documents\Ornella\Proyectos\tp-bbdd-aplicada\documentacion\Archivos para el TP\Inquilino-propietarios-UF.csv'
-	--exec ddbba.sp_relacionar_pagos
+	exec ddbba.sp_relacionar_inquilinos_uf  @ruta_archivo =
 end
 
 exec ddbba.sp_importar_archivos
