ba024187d87c549aeeb654cd94694895ab097acf
JimeBenitezz <jimebenitezuni@gmail.com>
2025-11-06 08:33:26 -0300

    Recreate sp_importar_uf_por_consorcios procedure
    Recreate the stored procedure for importing data from a file, including error handling and dynamic SQL execution.

--- entrega_5/sp/Exportar_todos_los_Archivos.sql
+++ entrega_5/sp/Exportar_todos_los_Archivos.sql
@@ -130,70 +130,9 @@ EXEC sp_MSset_oledb_prop 'Microsoft.ACE.OLEDB.12.0', 'AllowInProcess', 1;
 EXEC sp_MSset_oledb_prop 'Microsoft.ACE.OLEDB.12.0', 'DynamicParameters', 1;
 
 --para saber si sql encuentra el archivo
-EXEC xp_fileexist 'C:\Importar_TP\datos varios.xlsx';
+EXEC xp_fileexist 'C:\Importar_TP\datos varios.xlsx';*/
 -----------------------------------------------------------------------------
-CREATE OR ALTER PROCEDURE ddbba.sp_importar_uf_por_consorcios
-    @ruta_archivo NVARCHAR(255)
-AS
-BEGIN
-	create table #temp_UF
-	(
-		nom_consorcio VARCHAR(100),
-		num_UF INT,
-		piso VARCHAR (10),
-		departamento VARCHAR (10),
-		coeficiente VARCHAR(10),
-		m2_UF INT,
-		baulera CHAR(4),
-		cochera CHAR(4),
-		m2_baulera INT,
-		m2_cochera INT
-	)
 
-
-    -- Crear SQL dinámico para BULK INSERT
-    DECLARE @SQL NVARCHAR(MAX);
-    SET @SQL = N'
-        BULK INSERT #temp_UF
-        FROM ''' + @ruta_archivo + '''
-        WITH
-        (
-            FIELDTERMINATOR = ''\t'',   -- Tabulación
-            ROWTERMINATOR = ''\n'',
-            FIRSTROW = 2       
-        );';
-
-    -- Ejecutar SQL dinámico
-    BEGIN TRY
-        EXEC sp_executesql @sql;
-    END TRY
-    BEGIN CATCH
-        PRINT 'Error durante el BULK INSERT. Verifique la ruta del archivo, los permisos y el formato.';
-        PRINT ERROR_MESSAGE();
-        DROP TABLE IF EXISTS #temp_UF;
-        RETURN;
-    END CATCH
-	
-	INSERT INTO [ddbba].[unidad_funcional] (
-    id_unidad_funcional, id_consorcio, metros_cuadrados, piso, departamento, cochera, baulera, coeficiente
-    )
-    SELECT  
-        t.num_UF,
-        c.id_consorcio,
-        (t.m2_UF + t.m2_baulera + t.m2_cochera),
-        t.piso,
-        t.departamento,
-        CASE WHEN UPPER(LTRIM(RTRIM(t.cochera))) IN ('SI','SÍ') THEN 1 ELSE 0 END,
-        CASE WHEN UPPER(LTRIM(RTRIM(t.baulera))) IN ('SI','SÍ') THEN 1 ELSE 0 END,
-        TRY_CAST(REPLACE(t.coeficiente, ',', '.') AS DECIMAL(6,3))
-    FROM #temp_UF AS t
-    INNER JOIN ddbba.consorcio AS c
-        ON LTRIM(RTRIM(UPPER(c.nombre))) = LTRIM(RTRIM(UPPER(t.nom_consorcio)))
-
-	--ELIMINO LA TABLA TEMPORAL
-	DROP TABLE #temp_UF
-END
-GO*/
 --ESTO ES PARA EXPORTARLO CON CSV
 /*USE consorcios;
 GO
@@ -452,6 +391,69 @@ EXEC sp_MSset_oledb_prop 'Microsoft.ACE.OLEDB.12.0', 'DynamicParameters', 1;
 
 --para saber si sql encuentra el archivo
 EXEC xp_fileexist 'C:\Users\Usuario\Desktop\TPBASEDDATOS\documentacion\Archivos para el TP\datos varios.xlsx';*/
+-----------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE ddbba.sp_importar_uf_por_consorcios
+    @ruta_archivo NVARCHAR(255)
+AS
+BEGIN
+	create table #temp_UF
+	(
+		nom_consorcio VARCHAR(100),
+		num_UF INT,
+		piso VARCHAR (10),
+		departamento VARCHAR (10),
+		coeficiente VARCHAR(10),
+		m2_UF INT,
+		baulera CHAR(4),
+		cochera CHAR(4),
+		m2_baulera INT,
+		m2_cochera INT
+	)
+
+
+    -- Crear SQL dinámico para BULK INSERT
+    DECLARE @SQL NVARCHAR(MAX);
+    SET @SQL = N'
+        BULK INSERT #temp_UF
+        FROM ''' + @ruta_archivo + '''
+        WITH
+        (
+            FIELDTERMINATOR = ''\t'',   -- Tabulación
+            ROWTERMINATOR = ''\n'',
+            FIRSTROW = 2       
+        );';
+
+    -- Ejecutar SQL dinámico
+    BEGIN TRY
+        EXEC sp_executesql @sql;
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error durante el BULK INSERT. Verifique la ruta del archivo, los permisos y el formato.';
+        PRINT ERROR_MESSAGE();
+        DROP TABLE IF EXISTS #temp_UF;
+        RETURN;
+    END CATCH
+	
+	INSERT INTO [ddbba].[unidad_funcional] (
+    id_unidad_funcional, id_consorcio, metros_cuadrados, piso, departamento, cochera, baulera, coeficiente
+    )
+    SELECT  
+        t.num_UF,
+        c.id_consorcio,
+        (t.m2_UF + t.m2_baulera + t.m2_cochera),
+        t.piso,
+        t.departamento,
+        CASE WHEN UPPER(LTRIM(RTRIM(t.cochera))) IN ('SI','SÍ') THEN 1 ELSE 0 END,
+        CASE WHEN UPPER(LTRIM(RTRIM(t.baulera))) IN ('SI','SÍ') THEN 1 ELSE 0 END,
+        TRY_CAST(REPLACE(t.coeficiente, ',', '.') AS DECIMAL(6,3))
+    FROM #temp_UF AS t
+    INNER JOIN ddbba.consorcio AS c
+        ON LTRIM(RTRIM(UPPER(c.nombre))) = LTRIM(RTRIM(UPPER(t.nom_consorcio)))
+
+	--ELIMINO LA TABLA TEMPORAL
+	DROP TABLE #temp_UF
+END
+GO*
 ----------------------------------------------------------------------------------------------------------------
 
 ---PARA EXPORTAR INQUILINOS-PROPIETARIOS
