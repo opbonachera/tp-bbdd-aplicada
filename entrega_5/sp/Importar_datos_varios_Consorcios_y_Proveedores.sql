8e91b9ee8946237c2c94282991036f8ddcb5296c
JimeBenitezz <jimebenitezuni@gmail.com>
2025-11-06 20:09:54 -0300

    SP para exportar los archivos excel
    Refactor stored procedures for importing data from Excel files into the database. Updated temporary table creation and data insertion logic.

--- entrega_5/sp/Importar_datos_varios_Consorcios_y_Proveedores.sql
+++ entrega_5/sp/Importar_datos_varios_Consorcios_y_Proveedores.sql
@@ -1,181 +1,123 @@
 --Se tienen que descargar access para poder exportar el archivo sql https://www.microsoft.com/en-us/download/details.aspx?id=54920&msockid=0563dc410f8e6bbc023dc9f70ef26a4b
-use "consorcios"
-go 
-
+USE consorcios;
+GO
 CREATE OR ALTER PROCEDURE ddbba.sp_importar_consorcios
-    @NomArch VARCHAR(255)
+	@NomArch varchar(255)
 AS
 BEGIN
-    SET NOCOUNT ON;
+ 
+--creacion de la tabla temporal
+ CREATE TABLE #temp_consorcios
+ ( consorcio varchar(12),
+   nombre varchar(50),
+   domicilio varchar (50),
+   cant_UF smallint,
+   M2_totales int
+  );
+--inserto los datos del archivo excel a la tabla temporal con openrowset(lee datos desde un archivo)
+--Uso sql dinamico
+   DECLARE @sql NVARCHAR(MAX);
 
-    -- VARIABLES PARA SQL DINÁMICO
-    DECLARE @BulkSQL NVARCHAR(MAX);
-    DECLARE @DynamicSQL NVARCHAR(MAX);
-    
-    -- 0. Limpieza y Creación de la tabla temporal GLOBAL (##)
-    DROP TABLE IF EXISTS ##temp_consorcios; 
-    CREATE TABLE ##temp_consorcios
-     (
-        consorcio VARCHAR(12),
-        nombre VARCHAR(50),
-        domicilio VARCHAR(50),
-        cant_UF SMALLINT,
-        M2_totales INT
-     );
-    
-    -- 1. Definición y Ejecución del BULK INSERT (para cargar el archivo)
-    SET @BulkSQL = N'
-    BULK INSERT ##temp_consorcios
-    FROM ''' + @NomArch + N'''
-    WITH
-    (
-        FIELDTERMINATOR = '';'',  -- Asumo que tu CSV usa punto y coma
-        ROWTERMINATOR = ''0x0a'' -- Formato LF para archivos UNIX/Linux
-    );';
+    SET @sql = N'
+        INSERT INTO #temp_consorcios (consorcio, nombre, domicilio, cant_UF, M2_totales)
+        SELECT Consorcio, [Nombre del consorcio], Domicilio, [Cant unidades funcionales], [m2 totales]
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @NomArch + ';HDR=YES'',
+            ''SELECT * FROM [Consorcios$]''
+        );
+    ';
 
-    BEGIN TRY
-        -- Ejecuta la carga del archivo
-        EXEC sp_executesql @BulkSQL; 
-        
-    END TRY
-    BEGIN CATCH
-        PRINT 'Error durante el BULK INSERT. Verifique la ruta, permisos y formato.';
-        PRINT ERROR_MESSAGE();
-        DROP TABLE IF EXISTS ##temp_consorcios;
-        RETURN;
-    END CATCH
-    
-    -- 2. ELIMINACIÓN DE DUPLICADOS (AHORA DINÁMICO)
-    SET @DynamicSQL = N'
-        DELETE t
-        FROM ##temp_consorcios t  -- 
-        WHERE EXISTS (
-            SELECT 1
-            FROM ddbba.consorcio c
-            WHERE c.nombre = t.nombre
-        );';
-    EXEC sp_executesql @DynamicSQL; -- La compilación se difiere hasta este punto
+--ejecuto el sql dinamico
+    EXEC sp_executesql @sql;
 
-    -- 3. INSERCIÓN FINAL (AHORA DINÁMICO)
-    SET @DynamicSQL = N'
-        INSERT INTO ddbba.consorcio (
-            nombre,
-            metros_cuadrados,
-            direccion
-        )
-        SELECT
-            t.nombre,
-            t.M2_totales,  
-            t.domicilio
-        FROM ##temp_consorcios AS t;'; -- ¡CORREGIDO a ## y ejecutado dinámicamente!
-    EXEC sp_executesql @DynamicSQL; -- La compilación se difiere hasta este punto
-    
-    -- 4. Limpia la tabla temporal global al terminar
-    DROP TABLE IF EXISTS ##temp_consorcios;
+--Inserto los datos en la tabla original
+	INSERT INTO ddbba.consorcio(
+    nombre,
+    metros_cuadrados ,
+    direccion,
+	cant_UF
+	)
+	select
+	t.nombre,
+	t.M2_totales,
+	t.domicilio,
+	t.cant_UF
 
-END;
-GO
+	from #temp_consorcios as t
 
-
-
--- 6. Ejecución
-EXEC ddbba.sp_importar_consorcios
-    @NomArch = 'C:\pruebas\datos_varios(Consorcios).csv';
+	--elimino la tabla temporal
+	DROP TABLE #temp_consorcios
+END
 GO
 --Para ejecutar el SP
 EXEC ddbba.sp_importar_consorcios
-	@NomArch='C:\Users\Usuario\Desktop\TPBASEDDATOS\documentacion\Archivos para el TP\datos_varios(Consorcios).csv' --aca va la ruta de donde tengan el archivo
+	@NomArch='C:\Importar_TP\datos varios.xlsx' --aca va la ruta de donde tengan el archivo
 
 
 --Para ver si se inserto todo correctamente
 select * from ddbba.consorcio
-		
-------------------------------------------------------------------------------------------------------------------------
+
+
 ----SP PARA Importar_datos_varios_proveedores
-GO
-
-
-
-
 
 CREATE OR ALTER PROCEDURE ddbba.sp_importar_proveedores
-	@NomArch varchar(255) -- Debe contener la ruta COMPLETA del archivo CSV
+	@NomArch varchar(255)
 AS
 BEGIN
-    SET NOCOUNT ON;
-    
-    -- Variables para SQL Dinámico
-    DECLARE @BulkSQL NVARCHAR(MAX);
-    DECLARE @InsertSQL NVARCHAR(MAX); -- Nueva variable para el INSERT final
-    
-    -- 0. Limpieza y Creación de la tabla temporal GLOBAL (##)
-    DROP TABLE IF EXISTS ##temp_proveedores; 
-    
-    CREATE TABLE ##temp_proveedores
-     (	
-        tipo_de_gasto VARCHAR(50),
-        descripcion VARCHAR (100),
-        detalle VARCHAR(100) NULL,
-        nombre_consorcio VARCHAR (255)
-     );
+ 
+--creacion de la tabla temporal
+ CREATE TABLE #temp_proveedores
+ (  tipo_de_gasto VARCHAR(50),
+	descripcion VARCHAR (100),
+	detalle VARCHAR(100) NULL,
+	nombre_consorcio VARCHAR (255),
+  );
+--inserto los datos del archivo excel a la tabla temporal con openrowset(lee datos desde un archivo)
+--Uso sql dinamico
+   DECLARE @sql NVARCHAR(MAX);
 
-    -- 1. Definición y Ejecución del BULK INSERT
-    SET @BulkSQL = N'
-    BULK INSERT ##temp_proveedores
-    FROM ''' + @NomArch + N'''
-    WITH
-    (
-        FIELDTERMINATOR = '';'',     -- Asumo punto y coma
-        ROWTERMINATOR = ''0x0a'',  
-        FIRSTROW = 2                 -- Asumo que hay encabezados
-    );';
-
-    BEGIN TRY
-        -- Ejecuta la carga del archivo
-        EXEC sp_executesql @BulkSQL; 
-        
-    END TRY
-    BEGIN CATCH
-        PRINT 'Error durante el BULK INSERT. Verifique la ruta, permisos y formato.';
-        PRINT ERROR_MESSAGE();
-        DROP TABLE IF EXISTS ##temp_proveedores;
-        RETURN;
-    END CATCH
-    
-    -- 2. Inserto los datos de la tabla temporal a la tabla final (AHORA DINÁMICO)
-    --    Esto resuelve el error de "no se pudo enlazar" las columnas.
-
-    SET @InsertSQL = N'
-        INSERT INTO ddbba.Proveedores(
-            tipo_de_gasto,
-            descripcion,
-            detalle,
-            nombre_consorcio
-        )
-        SELECT
-            t.tipo_de_gasto,
-            t.descripcion,
-            t.detalle,
-            t.nombre_consorcio
-        FROM ##temp_proveedores AS t;
+    SET @sql = N'
+        INSERT INTO #temp_proveedores (tipo_de_gasto,descripcion,detalle,nombre_consorcio)
+        SELECT F1 as tipo_de_gasto, F2 as descripcion,F3 as detalle, F4 as nombre_consorcio
+        FROM OPENROWSET(
+            ''Microsoft.ACE.OLEDB.12.0'',
+            ''Excel 12.0;Database=' + @NomArch + ';HDR=NO'',
+            ''SELECT * FROM [Proveedores$]''
+        );
     ';
 
-    -- Ejecuta la inserción final
-    EXEC sp_executesql @InsertSQL; 
+--ejecuto el sql dinamico
+    EXEC sp_executesql @sql;
 
-    -- 3. Limpia la tabla temporal global
-    DROP TABLE IF EXISTS ##temp_proveedores;
-    
-END;
+--Inserto los datos en la tabla original
+	INSERT INTO ddbba.Proveedores(
+	tipo_de_gasto,
+	descripcion,
+	detalle,
+	nombre_consorcio
+	)
+	select
+	t.tipo_de_gasto,
+	t.descripcion,
+	t.detalle,
+	t.nombre_consorcio
+
+	from #temp_proveedores as t
+
+	--elimino la tabla temporal
+	DROP TABLE #temp_proveedores
+END
 GO
 
 --Para ejecutar el SP
 EXEC ddbba.sp_importar_proveedores
-	@NomArch='C:\pruebas\datos_varios(Proveedores).csv' --aca va la ruta de donde tengan el archivo(si o si tiene que ser en una carpeta disco c para los permisos)
+	@NomArch='C:\Importar_TP\datos varios.xlsx' --aca va la ruta de donde tengan el archivo
 
 --Para ver si se inserto todo correctamente
 select * from ddbba.proveedores
--------------------------------------------------------------------------------------------------------------------------------
+
+
 --ESTO ES POR SI ALGO NO LES ANDA
 --Habilita opciones avanzadas
 EXEC sp_configure 'show advanced options', 1;
@@ -188,4 +130,4 @@ EXEC sp_MSset_oledb_prop 'Microsoft.ACE.OLEDB.12.0', 'AllowInProcess', 1;
 EXEC sp_MSset_oledb_prop 'Microsoft.ACE.OLEDB.12.0', 'DynamicParameters', 1;
 
 --para saber si sql encuentra el archivo
-EXEC xp_fileexist 'C:\Users\Usuario\Desktop\TPBASEDDATOS\documentacion\Archivos para el TP\datos varios.xlsx';
+EXEC xp_fileexist 'â€ªC:\Importar_TP\Inquilino-propietarios-datos.csv';
