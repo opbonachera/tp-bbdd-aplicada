8e5ec320109feee5cd7850a334af301ccb0b77ff
roqueguardia <89049912+roqueguardia@users.noreply.github.com>
2025-11-13 14:44:24 -0300

    indices reporte 1

--- entrega-6/reportes/indices/indices.sql
+++ entrega-6/reportes/indices/indices.sql
@@ -1,3 +1,28 @@
+-- ======================================
+-- ÍNDICES PARA OPTIMIZAR sp_reporte_1
+-- ======================================
+
+-- 1️⃣ Pago: filtro por año y join con expensa
+CREATE INDEX IX_pago_fecha_expensa
+ON ddbba.pago (fecha_pago, id_expensa)
+INCLUDE (monto);
+
+-- 2️⃣ Expensa: join con pago y filtro por consorcio
+CREATE INDEX IX_expensa_consorcio
+ON ddbba.expensa (id_expensa, id_consorcio);
+
+-- 3️⃣ Gastos ordinarios: join por expensa
+CREATE INDEX IX_gastos_ordinarios_expensa
+ON ddbba.gastos_ordinarios (id_expensa, id_gasto_ordinario);
+
+-- 4️⃣ Gasto extraordinario: join por expensa
+CREATE INDEX IX_gasto_extraordinario_expensa
+ON ddbba.gasto_extraordinario (id_expensa, id_gasto_extraordinario);
+
+
+
+
+
 --Indices para el reporte 3
 
 -- Índice para mejorar filtros y joins en expensa
