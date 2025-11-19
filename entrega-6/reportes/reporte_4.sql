3dc2212a08161cfabc77c610d7aae6a1b0b79a70
Santiagoiiezequiel <santiago.arriola24@hotmail.com>
2025-11-19 03:21:25 -0300

    Arreglo reporte 4

--- entrega-6/reportes/reporte_4.sql
+++ entrega-6/reportes/reporte_4.sql
@@ -1,84 +1,11 @@
-<<<<<<< HEAD:entrega-6/reportes/reportes.sql
-
-
-create or alter procedure sp_reporte_1
-as
-begin
-    -- Creamos una CTE para calcular la recaudación semanal. De aca obtenemos: total de gastos ordinarios y extraordinarios por semana
-    WITH RecaudacionSemanal AS (
-        SELECT 
-            DATEPART(WEEK, p.fecha_pago) AS semana,
-            SUM(CASE WHEN go.id_gasto_ordinario IS NOT NULL THEN p.monto ELSE 0 END) AS total_ordinarios,
-            SUM(CASE WHEN ge.id_gasto_extraordinario IS NOT NULL THEN p.monto ELSE 0 END) AS total_extraordinarios
-        FROM ddbba.pago p
-        LEFT JOIN ddbba.expensa e ON e.id_expensa = p.id_expensa
-        LEFT JOIN ddbba.gastos_ordinarios go ON go.id_expensa = e.id_expensa
-        LEFT JOIN ddbba.gasto_extraordinario ge ON ge.id_expensa = e.id_expensa
-        GROUP BY DATEPART(WEEK, p.fecha_pago)
-    )
-    -- Obtenemos las columnas finales del reporte
-    SELECT 
-        semana,
-        total_ordinarios,
-        total_extraordinarios,
-        (total_ordinarios + total_extraordinarios) AS total_semanal,
-        AVG(total_ordinarios + total_extraordinarios) OVER () AS promedio_general,
-        SUM(total_ordinarios + total_extraordinarios) OVER (ORDER BY semana) AS acumulado_progresivo
-    FROM RecaudacionSemanal
-    ORDER BY semana;
-end;
-GO
-
-
-CREATE OR ALTER PROCEDURE ddbba.sp_reporte_2
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    DECLARE @cols NVARCHAR(MAX);
-    DECLARE @sql NVARCHAR(MAX);
-
-    -- 1. Obtener la lista de columnas (departamentos) dinámicamente
-    SELECT @cols = STRING_AGG(QUOTENAME(departamento), ',')
-    FROM (SELECT DISTINCT departamento FROM ddbba.unidad_funcional) AS d;
-
-    -- 2. Armar el SQL dinámico 
-    SET @sql = N'
-    WITH mes_uf_CTE AS (
-        SELECT 
-            FORMAT(p.fecha_pago, ''yyyy-MM'') AS mes, 
-            uf.departamento,
-            SUM(p.monto) AS total_monto
-        FROM ddbba.pago p  
-        JOIN ddbba.unidad_funcional uf  
-            ON uf.id_unidad_funcional = p.id_unidad_funcional
-        GROUP BY FORMAT(p.fecha_pago, ''yyyy-MM''), uf.departamento 
-    )
-    SELECT mes, ' + @cols + N'
-    FROM mes_uf_CTE -- Nombre del CTE actualizado aquí
-    PIVOT (
-        SUM(total_monto)
-        FOR departamento IN (' + @cols + N')
-    ) AS tabla_cruzada
-    ORDER BY mes;'; 
-
-    -- 3. Ejecutar la consulta dinámica
-    PRINT 'SQL dinámico a ejecutar:';
-    PRINT @sql;
-    EXEC sp_executesql @sql;
-END;
-GO
-    
-=======
-  
->>>>>>> 0eff548b4889cc629d0a75282308da28214e7a31:entrega-6/reportes/reporte_4.sql
-------------------------------------------------------------------------------------------------------
-/*Reporte 4 con XML
-Obtenga los 5 (cinco) meses de mayores gastos y los 5 (cinco) de mayores ingresos*/
+/*
+    Reporte 4. 
+        Obtenga los 5 (cinco) meses de mayores gastos y los 5 (cinco) de mayores ingresos. 
+*/
 CREATE OR ALTER PROCEDURE ddbba.sp_reporte_4
     @id_consorcio INT = NULL,  -- filtrar por consorcio
-    @AnioDesde INT = NULL,     -- año desde
-    @AnioHasta INT = NULL      --  año hasta
+    @AnioDesde INT = NULL,     -- a�o desde
+    @AnioHasta INT = NULL      --  a�o hasta
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -87,15 +14,16 @@ BEGIN
     DECLARE @FechaHasta DATE = NULL;
 
     -- Rango de fechas
+    --calcula el rango de las fechas segun el parametro pasado
     IF @AnioDesde IS NOT NULL
         SET @FechaDesde = DATEFROMPARTS(@AnioDesde, 1, 1);
     IF @AnioHasta IS NOT NULL
         SET @FechaHasta = DATEFROMPARTS(@AnioHasta, 12, 31);
 
 
--- TOP 5 MESES CON MAYORES GASTOS (Ordinarios + Extraordinarios)
-
+    -- TOP 5 MESES CON MAYORES GASTOS (Ordinarios + Extraordinarios)
     ;WITH GastosUnificados AS (
+        --se obtienen todos los gastos ordinarios segun los filtros pedios
         SELECT 
             YEAR(e.fecha_emision) AS Anio,
             MONTH(e.fecha_emision) AS Mes,
@@ -110,7 +38,7 @@ BEGIN
             AND (@FechaHasta IS NULL OR e.fecha_emision <= @FechaHasta)
 
         UNION ALL
-
+          --se obtienen todos los gastos extraordinarios segun los filtros pedios
         SELECT 
             YEAR(e.fecha_emision) AS Anio,
             MONTH(e.fecha_emision) AS Mes,
@@ -138,6 +66,7 @@ BEGIN
         FROM GastosUnificados
         GROUP BY Anio, Mes
     )
+    --se selecciona el top 5 de todos los gastos juntos (ordinarios y extraordinarios) y se lo convierte a xml
     SELECT TOP 5
         Anio AS [@Anio],
         Mes AS [@Mes],
@@ -154,8 +83,8 @@ BEGIN
     FOR XML PATH('Mes'), ROOT('Top5MesesGastos'), TYPE;
 
 
---  TOP 5 MESES CON MAYORES INGRESOS
-
+    --  TOP 5 MESES CON MAYORES INGRESOS
+    --aca se calcula el ingreso por mes teniendo en cuenta solo los pagos aprobados
     ;WITH IngresosMensuales AS (
         SELECT 
             YEAR(p.fecha_pago) AS Anio,
@@ -190,9 +119,3 @@ BEGIN
 
 END;
 GO
-
---TEST 
-EXEC ddbba.sp_reporte_4;-- sin parametros de entrada
-EXEC ddbba.sp_reporte_4 @id_consorcio = 5; --mandadole un consorcio
-EXEC ddbba.sp_reporte_4 @AnioDesde = 2025, @AnioHasta = 2025; --mandadole años
-EXEC ddbba.sp_reporte_4 @id_consorcio = 1, @AnioDesde = 2025, @AnioHasta = 2025;--mandadole todos los parametos
